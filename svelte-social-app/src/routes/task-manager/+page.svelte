3474a3537738b155d58ae704f917705137e47319
cowilliams_deloitte <cowilliams@deloitte.com>
2024-09-10 17:03:11 -0600

    almost done

---
+++ svelte-social-app/src/routes/task-manager/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	let count = 0;
+
+	$: {
+		if (count > 10) {
+			console.log('I am the winner');
+		}
+	}
+	$: doubled = count * 2;
+</script>
+
+<div class="text-white border-gray-100 p-2">
+	<div>
+		<p>I am the task manager page - {count}</p>
+	</div>
+	<div>
+		<p>doubled value: {doubled}</p>
+	</div>
+	<button class="text-green-500 p-2" on:click={() => count++}>Increment</button>
+	<button class="text-red-500 p-2" on:click={() => count--}>Decrement</button>
+</div>
