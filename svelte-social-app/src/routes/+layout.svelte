ca8c78ea49206906517af05df670c4ad1076f223
cowilliams_deloitte <cowilliams@deloitte.com>
2024-09-10 15:39:33 -0600

    installed tailwind and onfigured settings

---
+++ svelte-social-app/src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+    import '../App.css';
+</script>
+<div>I am a layout file</div>
+<slot />
---
+++ svelte-social-app/src/routes/profile/+page.svelte
@@ -0,0 +1,4 @@
+<h1 class="hey">I am the profile page </h1>
+<style>.hey{
+    color: green;
+}</style>
