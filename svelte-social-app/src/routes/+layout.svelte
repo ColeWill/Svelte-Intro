af5fd4e4ec1cd3961a485a651dd4a5c405167ce0
cowilliams_deloitte <cowilliams@deloitte.com>
2024-09-10 15:51:38 -0600

    css added

--- svelte-social-app/src/routes/+layout.svelte
+++ svelte-social-app/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
     import '../App.css';
 </script>
-<div>I am a layout file</div>
+<div class="text-teal-100">I am a layout file</div>
 <slot />
--- svelte-social-app/src/routes/+page.svelte
+++ svelte-social-app/src/routes/+page.svelte
@@ -2,4 +2,4 @@
     console.log('hello world')
 </script>
 
-<h1 class="test-3xl font-bold underline">Hello World</h1>
+<h1 class="text-3xl font-bold underline text-red-400">Hello World</h1>
